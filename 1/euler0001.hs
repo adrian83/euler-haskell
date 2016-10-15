3961955efd084d0c90c1f9ccfd3fad795e018500
adrian83 <adrianbrzoza1983@gmail.com>
2016-10-15 14:56:24 +0200

    1 fixed

--- 1/euler0001.hs
+++ 1/euler0001.hs
@@ -1,6 +1,6 @@
 
 mysum :: (Integral n) => n -> n
-mysum n = sum [x | x <- [1..n], (mod x 3) == 0 || (mod x 5) ==0]
+mysum n = sum [x | x <- [1..(n-1)], (mod x 3) == 0 || (mod x 5) ==0]
 
 main = do
     print (mysum 1000)
