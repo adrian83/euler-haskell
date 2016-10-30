88a3bd53648687edf463e49e0f61f1e95af3feb5
adrian83 <adrianbrzoza1983@gmail.com>
2016-10-30 12:25:38 +0100

    1 documented

--- 1/euler0001.hs
+++ 1/euler0001.hs
@@ -1,4 +1,11 @@
 
+-- https://projecteuler.net/problem=1
+-- 
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5,
+-- we get 3, 5, 6 and 9. The sum of these multiples is 23.
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
+
 mysum :: (Integral n) => n -> n
 mysum n = sum [x | x <- [1..(n-1)], (mod x 3) == 0 || (mod x 5) ==0]
 
