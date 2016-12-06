ebf84579a74d291bef779b61966fa7e447c5f716
Adrian Brzoza <adrian.brzoza@siili.com>
2016-12-06 10:56:08 +0100

    9

---
+++ 9/euler0009.hs
@@ -0,0 +1,16 @@
+-- https://projecteuler.net/problem=9
+--
+-- A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+-- a^2 + b^2 = c^2
+-- For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+-- There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+-- Find the product abc.
+
+-- pythagoreanTriplets returns list of pythagorean triplets for which sum of all edges is equal to 1st arg.
+pythagoreanTriplets :: Integer -> [[Integer]]
+pythagoreanTriplets s = [[a,b,c] | a <- [1,2..s], b <- [2,3..s], c <- [3,4..s], c > b, b > a, c^2 == a^2 + b^2, s == c + a + b]
+
+
+main :: IO ()
+main = do
+  print( product $ head $ pythagoreanTriplets 1000 )
