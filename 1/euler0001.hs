a3f3d4433cb0e845365a18bebd2397749dee96dc
adrian83 <adrianbrzoza1983@gmail.com>
2017-02-10 22:13:58 +0100

    1 updated

--- 1/euler0001.hs
+++ 1/euler0001.hs
@@ -5,12 +5,11 @@
 -- we get 3, 5, 6 and 9. The sum of these multiples is 23.
 -- Find the sum of all the multiples of 3 or 5 below 1000.
 
-maxx :: Integer
-maxx = 1000
 
-result :: Integer
-result = sum [x | x <- [1..(maxx-1)], (mod x 3) == 0 || (mod x 5) ==0]
+result :: Integer -> Integer
+result maxNumber = sum [x | x <- [1..(maxNumber-1)], mod x 3 == 0 || mod x 5 ==0]
 
 main :: IO ()
 main = do
-  print (result)
+  print (result 10)
+  print (result 1000)
