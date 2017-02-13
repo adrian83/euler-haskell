49700ce9ed80296e9b9c38d5f01100dbe795f9d2
adrian83 <adrianbrzoza1983@gmail.com>
2017-02-13 22:04:24 +0100

    21

---
+++ 21/euler0021.hs
@@ -0,0 +1,33 @@
+-- https://projecteuler.net/problem=21
+--
+-- Let d(n) be defined as the sum of proper divisors of n (numbers less
+-- than n which divide evenly into n).
+-- If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair
+-- and each of a and b are called amicable numbers.
+-- For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22,
+-- 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284
+-- are 1, 2, 4, 71 and 142; so d(284) = 220.
+-- Evaluate the sum of all the amicable numbers under 10000.
+
+dividors :: Integer -> [Integer]
+dividors a = [ i | i <- [1,2..(quot a 2)], mod a i == 0]
+
+result :: Integer -> Integer -> Integer
+result 0 acc = acc
+result a acc =
+  let
+    b = sum (dividors a)
+  in
+    if b < 10000 && b < 10000 && a /= b && a == (sum (dividors b)) then result (a-1) (acc+a+b) else result (a-1) acc
+
+
+
+
+main :: IO ()
+main = do
+  print (dividors 220)
+  print (sum $ dividors 220)
+  print (dividors 284)
+  print (sum $ dividors 284)
+  print (result 15000 0)
+  -- print(result 10000 0)
