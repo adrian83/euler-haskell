2918eeba3b0b1925811f90b267ef7c802985219d
adrian83 <adrianbrzoza1983@gmail.com>
2017-02-15 17:59:39 +0100

    performance issue fixed

--- 7/euler0007.hs
+++ 7/euler0007.hs
@@ -3,16 +3,14 @@
 -- By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 -- What is the 10 001st prime number?
 
-divisable :: Integer -> [Integer] -> Bool
-divisable _ [] = False
-divisable n (x:xs) = if mod n x == 0 then True else divisable n xs
-
-prime :: Integer -> Integer -> [Integer] -> Integer
-prime _ 0 primes = head primes
-prime c n primes = if divisable c primes then prime (c+1) n primes else prime (c+1) (n-1) (c:primes)
+prime :: [Integer] -> Int -> Integer
+prime primes e = last $ take e primes
 
 
 main :: IO ()
 main = do
-  print ( prime 3 5 [2] )
-  print ( prime 3 10000 [2] )
+  f <- readFile "../primes/primes"
+  let primes = [read s :: Integer | s <- lines f]
+
+  print (prime primes 6)
+  print (prime primes 10001)
