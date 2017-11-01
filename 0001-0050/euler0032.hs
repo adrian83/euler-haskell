3c332d2d4c6c275cde8e6997aae80d84ecf27f2c
adrian83 <adrianbrzoza1983@gmail.com>
2017-11-01 14:38:20 +0100

    32 refactored

--- 0001-0050/euler0032.hs
+++ 0001-0050/euler0032.hs
@@ -8,38 +8,6 @@
 
 import Data.List
 
-
-bounds :: [([Integer], [Integer])]
-bounds = [([1..9],[1000..9999]), ([10..99],[100..999])]
-
-products :: [([Integer], [Integer])] -> [Product]
-products [] = []
-products bnds = [Product {a=a, b=b, axb=a*b, str=((show a) ++ (show b) ++ (show (a * b)))} | a <- fst (head bnds), b <- snd (head bnds)] ++ (products $ tail bnds)
-
-gg :: Int -> String -> Bool
-gg 0 _ = False
-gg 1 str = str == "1"
-gg 2 str = str == "12"
-gg 3 str = str == "123"
-gg 4 str = str == "1234"
-gg 5 str = str == "12345"
-gg 6 str = str == "123456"
-gg 7 str = str == "1234567"
-gg 8 str = str == "12345678"
-gg 9 str = str == "123456789"
-gg _ _ = False
-
-
-len :: Product -> Int
-len p = length $ str p
-
-
-
-pandigital :: Product -> Bool
-pandigital p = let
-  sortedStr = sort $ str p
-  in gg (length sortedStr) sortedStr
-
 data Product = Product {
     a :: Integer,
     b :: Integer,
@@ -47,14 +15,37 @@ data Product = Product {
     str :: String
   } deriving (Show)
 
+pandigital :: Product -> Bool
+pandigital p = case length (str p) of
+      0 -> False
+      1 -> sorted == "1"
+      2 -> sorted == "12"
+      3 -> sorted == "123"
+      4 -> sorted == "1234"
+      5 -> sorted == "12345"
+      6 -> sorted == "123456"
+      7 -> sorted == "1234567"
+      8 -> sorted == "12345678"
+      9 -> sorted == "123456789"
+      _ -> False
+      where sorted = sort $ str p
+
+products :: [([Integer], [Integer])] -> [Product]
+products [] = []
+products bnds = [Product {a=a, b=b, axb=a*b, str=((show a) ++ (show b) ++ (show (a * b)))} | a <- fst (head bnds), b <- snd (head bnds)] ++ (products $ tail bnds)
+
 unique :: [Integer] -> [Integer] -> [Integer]
 unique [] acc = acc
 unique numbers acc = if (head numbers) `elem` acc then unique (tail numbers) acc else unique (tail numbers) ((head numbers):acc)
 
 
+bounds :: [([Integer], [Integer])]
+bounds = [([1..9],[1000..9999]), ([10..99],[100..999])]
+
+result :: [([Integer], [Integer])] -> Integer
+result bnds = sum (unique [ axb p | p <- (filter pandigital (products bnds))] [])
+
+
 main :: IO ()
 main = do
-    print (sum (unique [ axb p | p <- (filter (\p -> len p == 9 && pandigital p) (products bounds))] []))
-
-  --print (take 3  (filter (\p -> len p == 9 && pandigital p) (products bounds)))
-  --print (sum [ axb p | p <- (filter (\p -> len p == 9 && pandigital p) (products bounds))])
+    print ("Expected: " ++ show (45228 :: Integer) ++ ", actual: " ++ show (result bounds))
